02b971f7c9e938d73f90202d7b92b29e410b572d
RKNSYAH <115148049+RKNSYAH@users.noreply.github.com>
2023-12-04 14:12:20 +0700

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,13 @@ const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const Route = require("./routes/Route.js");
 const db = require("./config/Database.js");
-
+require('dotenv').config()
 const app = express()
 const PORT = 5000;
 
 app.use(cors({
-    origin: "*"
+    origin: "http://localhost:5173",
+    credentials: true,
 }));
 app.use(express.json());
 app.use(fileUpload());
